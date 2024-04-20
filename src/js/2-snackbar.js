e5921435db686ec8dcc2b5a346d18273c09b2840
Rimma1992 <149500783+Rimma1992@users.noreply.github.com>
2024-04-20 21:55:35 +0200

    update

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,49 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import done from '../img/done.svg';
+import error from '../img/error.svg';
+
+const form = document.querySelector('.form-delay');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const delayInput = +event.currentTarget.elements.delay.value;
+  const stateInput = event.currentTarget.elements.state.value;
+
+  createPromiseWithDelay(stateInput, delayInput)
+    .then(delay => {
+      iziToast.success({
+        message: `Fulfilled promise in ${delay}ms`,
+        iconUrl: done,
+        backgroundColor: '#59A10D',
+        messageColor: '#fff',
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        message: `Rejected promise in ${delay}ms`,
+        iconUrl: error,
+        backgroundColor: '#EF4040',
+        messageColor: '#fff',
+        position: 'topRight',
+      });
+    });
+
+  event.currentTarget.reset();
+}
+
+function createPromiseWithDelay(btn, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (btn === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
